82bc10466cdd68df5e92792cb67b835a9bc5c048
igarasy <lucas.igarashi@caju.com.br>
2023-08-09 17:44:35 -0300

    feat: create widgets

---
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -0,0 +1,23 @@
+import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
+import 'package:flutter/material.dart';
+import '../../models/expense.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({
+    super.key,
+    required this.expenses,
+  });
+
+  final List<Expense> expenses;
+
+  //when whe have lists that we dont know the length its better to use ListView.builder()
+  //the builder will create the element only when they are visible
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (ctx, index) => ExpenseItem(expenses[index]),
+    );
+  }
+}
